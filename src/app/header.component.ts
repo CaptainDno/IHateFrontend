d934df3388baaab3adb196f74565dd81ed806859
CaptainDno <75518941+CaptainDno@users.noreply.github.com>
2022-04-25 17:21:20 +0400

    Mobile devices support

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -6,9 +6,9 @@ import { isMobileLayout } from "./app.component";
     selector: "header-component",
     template: `
         <div>
-            <nav>
+            <nav style="display: flex; flex-direction: row; gap: 30px;">
                 <a class="a title" routerLink="/about">Страна Черногория</a>
-                <div style="display: flex; align-items: center; flex-wrap:wrap; gap: 20px; margin-left: 30px;">
+                <div style="display: flex; align-items: center; flex-direction: row; flex-wrap:wrap; gap: 15px;">
                     <a *ngFor="let route of routes" routerLink="/{{route.path}}"><span routerLinkActive="active" >{{route.data[0]}}</span></a>
                     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><span>О себе</span></a>
                 </div>
