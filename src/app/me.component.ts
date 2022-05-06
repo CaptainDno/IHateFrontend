db7a074a7452e32d6646c6827026edc505895773
CaptainDno <75518941+CaptainDno@users.noreply.github.com>
2022-05-06 21:45:31 +0400

    fixed about me

--- src/app/me.component.ts
+++ src/app/me.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'me-component',
     template: `
-    <div style="display: flex; flex-direction: column; gap: 10px;">
+    <div style="display: flex; flex-direction: column; gap: 10px; padding: 50px;">
         <div>
             <p>Вообще здесь должен быть рикролл. Но эти требования к оформлению портят вообще всё. Так что я вынужден написать тут что-то про себя, и вариант "я - дерьмо" не прокатывает.</p>
             <p>Ну в первых самое главное: я - дерьмо; учусь на <span title="06.05.2022">данный момент</span> в 9 "В" классе. Сей сайт был сделан за два с половиной дня и он тоже дерьмо.</p>
